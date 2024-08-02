4479ab4dd8e0ea47016a5f3aa66969e55a89b291
JazzMedo <84742298+Jazzmedo@users.noreply.github.com>
2024-08-02 17:14:39 +0300

    2.0

--- src/components/Item/Networks.jsx
+++ src/components/Item/Networks.jsx
@@ -18,17 +18,17 @@ function Networks() {
     }, [type, id, title, data, release])
 
 
-    function manipulateString(str="") {
+    function manipulateString(str="",by="-") {
         str = str.toLowerCase()
         // Replace spaces with hyphens
-        str = str.replace(/\s+/g, '-');
+        str = str.replace(/\s+/g, by);
         str = str.replace(":", '');
         str = str.replace(",", '');
 
         // Replace '&' with 'and'
         str = str.replace('&', 'and');
-        str = str.replace("--", '-');
-        str = str.replace("---", '-');
+        str = str.replace("--", by);
+        str = str.replace("---", by);
         return str;
     }
     function getGenres() {
@@ -48,15 +48,20 @@ function Networks() {
                 <a className='fgsdasd' target='_blank' href={`https://ext.to/search/?q=${title.split("-").join("+")}+${type == 'movie' ? release : ""}`}>
                     <img className='netw' src={`https://ext.to/static/img/ext_logo.png`} />
                 </a>
+                {type == 'movie' ? 
+                <a className='fgsdasd' target='_blank' href={`https://yts.mx/movies/${title}${type == 'movie' ? "-"+release : ""}`}>
+                    <img style={{ backgroundColor: '#000' }} className='netw' src={`https://yts.mx/assets/images/website/logo-YTS.svg`} />
+                </a> : <></>}
+                <a className='fgsdasd' target='_blank' href={`https://pahe.ink/?s=${title.split("-").join("+")}`}>
+                    <img className='netw' style={{ padding: '0px' }} src={require('../../logo/pahe.png')} />
+                </a>
+                <a className='fgsdasd' target='_blank' href={`https://psa.wf/?s=${title.split("-").join("+")}`}>
+                    <img className='netw' style={{ padding: '0px' }} src={require('../../logo/psa.png')} />
+                </a>
                 <a className='fgsdasd' target='_blank' href={`https://subsource.net/subtitles/${title}${type == 'movie' ? "-"+release : ""}`}>
                     <img style={{ backgroundColor: '#000' }} className='netw' src={`https://subsource.net/static/media/logo_full_dark.5addecabd16c37b4c784.png`} />
                 </a>
             </div>
-            {Array.isArray(data.networks) ? data.networks.map(net => (
-                <div key={net.id} className="flexonlyy">
-                    <img className='netw' src={`https://image.tmdb.org/t/p/original/${net.logo_path}`} />
-                </div>
-            )) : <></>}
         </div>
     )
 }
--- src/components/Season/SeasPost.jsx
+++ src/components/Season/SeasPost.jsx
@@ -1,9 +1,30 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { SeasonContext } from '../context/SeasonContext'
 import { Link } from 'react-router-dom'
 
 function SeasPost() {
     let { id, episodes, back, logo } = useContext(SeasonContext)
+    let [title, setTitle] = useState('')
+    useEffect(() => {
+        let x = back.name || episodes.name
+
+        setTitle(manipulateString(x))
+    }, [back])
+
+    function manipulateString(str = "") {
+        str = str.toLowerCase()
+        // Replace spaces with hyphens
+        str = str.replace(/\s+/g, '-');
+        str = str.replace(":", '');
+        str = str.replace(",", '');
+
+        // Replace '&' with 'and'
+        str = str.replace('&', 'and');
+        str = str.replace("--", '-');
+        str = str.replace("---", '-');
+        return str;
+    }
+
     console.log(episodes)
     return (
         <>
@@ -22,6 +43,15 @@ function SeasPost() {
                 <div className="flexonlyy">
                     <a className='fgsdasd' href={back.name ? `https://ext.to/search/?q=${back.name.split(" ").join("+")}+S${+episodes.season_number < 10 ? "0" + episodes.season_number : episodes.season_number}` : '#'}>
                         <img className='netw' src={`https://ext.to/static/img/ext_logo.png`} />
+                    </a>
+                    <a className='fgsdasd' target='_blank' href={`https://subsource.net/subtitles/${title}/season-${episodes.season_number}`}>
+                        <img style={{ backgroundColor: '#000' }} className='netw' src={`https://subsource.net/static/media/logo_full_dark.5addecabd16c37b4c784.png`} />
+                    </a>
+                    <a className='fgsdasd' target='_blank' href={`https://pahe.ink/?s=${title.split("-").join("+")}`}>
+                        <img className='netw' style={{ padding: '0px' }} src={require('../../logo/pahe.png')} />
+                    </a>
+                    <a className='fgsdasd' target='_blank' href={`https://psa.wf/?s=${title.split("-").join("+")}`}>
+                        <img className='netw' style={{ padding: '0px' }} src={require('../../logo/psa.png')} />
                     </a>
                 </div>
             </div>
